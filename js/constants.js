291eb7c89b6440b5a61965f96b954c90993fa45b
Turn Off the Lights <turnoffthelights@users.noreply.github.com>
2019-12-22 13:23:44 +0100

    v4.1.3.0

--- js/constants.js
+++ js/constants.js
@@ -1,12 +1,15 @@
 function $(id) { return document.getElementById(id); }
 // Install on www.stefanvd.net
 // Install on www.turnoffthelights.com
-if (window.location.href.match(/http:\/\/(.*stefanvd\.net\/.*|www\.stefanvd\.net\/.*\/.*)/i)|| window.location.href.match(/http:\/\/(.*turnoffthelights\.com\/.*|www\.turnoffthelights\.com\/.*\/.*)/i)){
-	if ($('turnoffthelights-firefox-install-button')) {
+if(window.location.href.match(/^http(s)?:\/\/(www\.)?stefanvd.net/i)||window.location.href.match(/^http(s)?:\/\/(www\.)?turnoffthelights.com/i)){
+	if($('turnoffthelights-firefox-install-button')){
 		$('turnoffthelights-firefox-install-button').style.display = 'none';
-		$('turnoffthelights-firefox-thanks-button').style.display = '';
+		$('turnoffthelights-firefox-thanks-button').style.display = 'block';
 	}
 }
+var totlscreenshotpage = "https://www.turnoffthelights.com/extension/capture-screenshot-of-video.html";
+var developerwebsite = "https://www.turnoffthelights.com";
+var totloptionspage = "https://www.turnoffthelights.com/extension/options.html";
 var ambientaureaproduct = "https://addons.mozilla.org/firefox/addon/ambient-aurea";
 var datetodayproduct = "https://addons.mozilla.org/firefox/addon/date-today";
 var turnoffthelightsproduct = "https://addons.mozilla.org/firefox/addon/turn-off-the-lights";
@@ -27,3 +30,6 @@ var linkthemedownload = "https://www.turnoffthelights.com/browser/theme.html";
 var browsernewtab = "null";
 var browserstore = "https://addons.mozilla.org";
 var linkyoutube = "https://www.youtube.com/c/turnoffthelights?sub_confirmation=1";
+var linkauroraplayerapp = "https://www.stefanvd.net/project/aurora-player/";
+var linktotlmobileapp = "https://www.turnoffthelights.com/mobile.html";
+var devmode = false;
